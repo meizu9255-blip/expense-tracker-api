3d9d69d9773923315ab4023ed7be2b554b69de32
Beksultan <meizu9255@gmail.com>
2025-11-28 10:25:45 -0600

    Switch to sha256_crypt

--- app/utils.py
+++ app/utils.py
@@ -1,13 +1,10 @@
 from passlib.context import CryptContext
 
-# Бұл жерде bcrypt орнына SHA256-crypt қолданамыз
+# Bcrypt орнына sha256_crypt қолданамыз (Бұл Render-де қатесіз істейді)
 pwd_context = CryptContext(schemes=["sha256_crypt"], deprecated="auto")
 
 def get_password_hash(password: str) -> str:
-    """Парольді алып, хешке айналдырады (SHA256 арқылы)"""
-    # SHA256 ұзындықты өзі реттейді, сондықтан қысқарту қажет емес
     return pwd_context.hash(password)
 
 def verify_password(plain_password, hashed_password):
-    """Енгізілген пароль мен базадағы хешті салыстырады"""
     return pwd_context.verify(plain_password, hashed_password)
