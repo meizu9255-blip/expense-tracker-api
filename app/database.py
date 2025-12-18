1fb169f1609f8800b19e1fd12f2d42a2b13db38a
Beksultan <meizu9255@gmail.com>
2025-12-18 11:48:20 -0600

    Initial commit: Qarjy Pro MVP version

--- app/database.py
+++ app/database.py
@@ -3,11 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not SQLALCHEMY_DATABASE_URL:
-    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost:5432/expense_db"
+    SQLALCHEMY_DATABASE_URL = "postgresql://my_expense_db_o2kd_user:Tqzj7wdzfTwL9L6quslQ6SiEcjLZZ9VH@dpg-d4utq0a4d50c73bkqg2g-a.oregon-postgres.render.com:5432/my_expense_db_o2kd"
 
 if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
